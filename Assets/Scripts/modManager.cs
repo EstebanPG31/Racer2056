d120345c43cb7703138c0349d7eb14151cfcb94f
EstebanPG31 <per.glez.e@gmail.com>
2024-05-28 21:37:21 -0600

    Añadidos Mods Visuales
    Se añaden los Mods para probar los cambios visuales mediante script

--- Assets/Scripts/modManager.cs
+++ Assets/Scripts/modManager.cs
@@ -11,37 +11,67 @@ public class modManager : MonoBehaviour
     private int modCount = 0;
     public GameObject[] modGameObjects;
     //public Sprite[] modSprites;   
-    //public Image activeMod;
+    //public Image activeMod1;
+    //public Image activeMod2;
+    //public Image activeMod3;
+    private int currentMod = 0;
+    private int ownedMod1 = 10;
+    private int ownedMod2 = 20;
+    private int ownedMod3 = 30;
 
     private void Update()
     {
         Debug.Log(opener.ToString());
-        slotController();
+        if (modCount > 0)
+        {
+            slotController();
+        }
+            
     }
-    private void slotController()
+    public void slotController()
     {
-        if (Input.GetKeyDown(KeyCode.L) && opener<4)
-        {
-            opener += 1;
-            if (opener > 3)
-                opener = 1;
-            slotsAnimator.SetInteger("Slot", opener);
-        }
-        if (Input.GetKeyDown(KeyCode.J) && opener > 0)
-        {
-            opener -= 1;
-            if (opener < 1)
-                opener = 3;
-            slotsAnimator.SetInteger("Slot", opener);
-        }
+        //opener = modGameObjects[currentMod].GetComponent
     }
 
-    /*private IEnumerator OnTriggerEnter(Collider other)
+    private IEnumerator OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Dron")
         {
             other.gameObject.GetComponent<BoxCollider>().enabled = false;
             other.gameObject.GetComponent<Animator>().SetBool("Available", false);
         }
-    }*/
+        if (modCount < 3)
+        {
+            StartCoroutine(ModAsign());
+        }
+
+        yield return new WaitForSeconds(1);
+        other.gameObject.GetComponent<Animator>().SetBool("Available", true);
+        other.gameObject.GetComponent<BoxCollider>().enabled = true;
+    }
+
+    public IEnumerator ModAsign()
+    {
+        if (modCount == 0)
+        {
+            ownedMod1 = Random.Range(0, modGameObjects.Length);
+            currentMod = ownedMod1;
+            //activeMod1.sprite = modSprites[ownedMod1]
+            yield return new WaitForSeconds(1);
+            modGameObjects[currentMod].SetActive(true);
+        }
+        else if (modCount == 1)
+        {
+            ownedMod2 = Random.Range(0, modGameObjects.Length);
+            //activeMod2.sprite = modSprites[ownedMod2]
+            yield return new WaitForSeconds(1);
+        }
+        else if(modCount == 2)
+        {
+            ownedMod3 = Random.Range(0, modGameObjects.Length);
+            //activeMod3.sprite = modSprites[ownedMod3]
+            yield return new WaitForSeconds(1);
+        }
+        modCount++;
+    }
 }
